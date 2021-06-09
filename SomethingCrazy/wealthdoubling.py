8236ace572123c8b94511461352458883567e424
venkata subbaraju <subbaraju@gmail.com>
2021-06-09 09:23:50 +0530

    Add wealthdoubling.py file.

--- SomethingCrazy/wealthdoubling.py
+++ SomethingCrazy/wealthdoubling.py
@@ -1,10 +1,12 @@
 # Program to calculate daily wealth to multiply your wealth  in a given number of days
 # yeah I know...I am greedy. But have fun
-wealth_multiplier = float(input(" How many times your wealth should be improved? "))
-days = int(input(" You want your wealth to be multiplied by " + str(wealth_multiplier) +
-                 " times and in how many days? "))
-name = input(" Oh by the way what is your name? ")
+name = input("Hey! first what is your name? ")
+wealth_multiplier = float(input(name + "! How many times your wealth should be improved? "))
+days = int(input("Okay! In how many days? "))
 # Actual calculations start here
 daily_percent = (float(wealth_multiplier**(1/days)) - 1)*100
-print("Subbaraju! you daily need to increase your wealth by " + str(daily_percent) +
-       "% inorder to multiply your wealth by " + str(wealth_multiplier) + " times in " + str(days) + " days")
+print("Hey " + name + "! So if you daily increase your wealth approximately by " + str(round(daily_percent)) +
+      "% you will multiply your wealth by " + str(wealth_multiplier) + " times in " + str(days) + " days")
+current_wealth = int(input("Give me your current wealth rounded of rupees: "))
+print(" If you daily increase it by " + str(round(daily_percent)) +
+      "% then it will be " + str(wealth_multiplier*current_wealth) + " in just " + str(days) + " days. Ciao!")
